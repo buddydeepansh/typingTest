8293d265999b93ab1b4a2d31520cd5f235cae8f4
deepansh <99.deepanshagrawal@gmail.com>
2023-06-10 21:51:24 +0530

    * Added google auth.

--- src/Components/AccountCircle/AccountCircle.jsx
+++ src/Components/AccountCircle/AccountCircle.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react"
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded"
 import "./AccountCircle.css"
-import { Modal, Tab, Tabs } from "@mui/material"
+import { Box, Modal, Tab, Tabs } from "@mui/material"
 import AppBar from "@mui/material/AppBar"
 import LoginForm from "../LoginForm/LoginForm"
 import SignUpForm from "../SignUpForm/SignUpForm"
 import { useTheme } from "../../Context/ThemeContext"
+import GoogleButton from "react-google-button"
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { auth } from "../../firebaseConfig"
+import { errorMapping } from "../../Utils/ErrorMapping"
+import { toast } from "react-toastify"
+
 const AccountCircle = () => {
   const [open, setOpen] = useState(false)
   const [valueTabs, setValueTabs] = useState(0)
@@ -13,6 +19,34 @@ const AccountCircle = () => {
   const handleTabChange = (e, v) => {
     // console.log(e, v)
     setValueTabs(v)
+  }
+  const handleGoogleSign = async (e) => {
+    const provider = new GoogleAuthProvider()
+    try {
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+      toast.success("Google Log In Successful", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      })
+    } catch (error) {
+      toast.error(errorMapping[error.code] || "Some error occured", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      })
+    }
   }
   return (
     <div>
@@ -37,6 +71,10 @@ const AccountCircle = () => {
           </AppBar>
           {valueTabs === 0 && <LoginForm />}
           {valueTabs === 1 && <SignUpForm />}
+          <Box className={"googleBox"}>
+            <span>OR</span>
+            <GoogleButton className="googleBTN" onClick={handleGoogleSign} />
+          </Box>
         </div>
       </Modal>
     </div>
