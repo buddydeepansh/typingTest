b43903d4fd3b3cfbd0ebe081487edd417942d3ae
deepansh <99.deepanshagrawal@gmail.com>
2023-06-12 21:32:04 +0530

    * Storing stats in firedb.

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -1,4 +1,8 @@
-import React from "react"
+import { addDoc, collection } from "firebase/firestore"
+import React, { useEffect } from "react"
+import { toast } from "react-toastify"
+import { errorMapping } from "../../Utils/ErrorMapping"
+import { auth, db } from "../../firebaseConfig"
 import Graph from "../Graph/Graph"
 import "./Stats.css"
 
@@ -10,6 +14,59 @@ const Stats = ({ wpm, accuracy, correctCharacters, incorrectCharacters, missedCh
       return i
     }
   })
+
+  const pushDatatoDB = async () => {
+    const { uid } = auth.currentUser
+    addDoc(collection(db, "Results"), {
+      wpn: wpm,
+      accuracy: accuracy,
+      timeStamp: new Date(),
+      characters: `${correctCharacters} / ${incorrectCharacters} / ${missedChars} / ${extraChars}`,
+      userId: uid,
+    })
+      .then(() => {
+        toast.success("Data saved in DB", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: false,
+          progress: undefined,
+          theme: "light",
+        })
+      })
+      .catch((error) => {
+        toast.error(errorMapping[error.code] || "Some error occured", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: false,
+          progress: undefined,
+          theme: "light",
+        })
+      })
+  }
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      pushDatatoDB()
+    } else {
+      toast.warn("Login / Signup to save results", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      })
+    }
+  }, [])
+
   return (
     <div className="stats-box">
       <div className="left-stats">
